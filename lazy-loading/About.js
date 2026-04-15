94a0475d524a1982db59a5004ea77501436bbdf5
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-15 12:05:00 +0530

    Create About.js

---
+++ lazy-loading/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="page-card">
+      <h2>ℹ️ About Page</h2>
+      <p>This page is loaded only when required.</p>
+    </div>
+  );
+}
+
+export default About;
